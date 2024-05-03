301aeac15bbe42d24f50d6e76ea56550c9618ac5
daedalus <manny.is.daedalus@pm.me>
2024-05-03 05:38:00 +0000

    tasks 0 to 4

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,13 @@
+const spy = require('sinon').spy;
+const sendPaymentRequestToApi = require('./3-payment');
+const utils = require('./utils');
+const expect = require('chai').expect;
+
+describe('calculateNumber', () => {
+  it('validate Utils.calculateNumber against sendPaymentRequestToApi', () => {
+    const spyCheck = spy(utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(spyCheck.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    spyCheck.restore();
+  });
+});
